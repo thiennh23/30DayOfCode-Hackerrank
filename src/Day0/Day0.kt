b6cdea9f3b2fa46a5fe7d7f9cd8a50e0a2f71365
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2024-07-13 14:31:51 +0700

    Day0: Hello world

---
+++ src/Day0/Day0.kt
@@ -0,0 +1,11 @@
+package Day0
+
+//Your Day 0: Hello, World.
+
+fun main() {
+    println("Hello, World.")
+
+    //Read a string form keyword
+    val inputString = readln()
+    println(inputString)
+}
