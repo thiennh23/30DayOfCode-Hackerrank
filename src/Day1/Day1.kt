0abc5a32d6e7e527a5bbe24a68b74d4a85c5cc7f
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2024-07-14 09:58:09 +0700

    Day2

---
+++ src/Day1/Day1.kt
@@ -0,0 +1,10 @@
+package Day1
+
+fun main() {
+    var i = readln()!!.toInt()
+    var d = readln().toDouble()
+    var s = readln()!!
+    println(i + d)
+    println(d + d)
+    println("HackerRank $s")
+}
