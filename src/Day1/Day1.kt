55094a34df2ce4d88012376bc67630d345bb1a74
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2024-07-14 09:59:55 +0700

    fixing

--- src/Day1/Day1.kt
+++ src/Day1/Day1.kt
@@ -1,10 +1,15 @@
 package Day1
 
 fun main() {
-    var i = readln()!!.toInt()
-    var d = readln().toDouble()
-    var s = readln()!!
-    println(i + d)
-    println(d + d)
-    println("HackerRank $s")
+
+    var i = 4;
+    var d = 4.0;
+    var s = "HackerRank ";
+
+    var i2 = readln()!!.toInt()
+    var d2 = readln().toDouble()
+    var s2 = readln()!!
+    println(i + i2)
+    println(d + d2)
+    println(s + s2)
 }
