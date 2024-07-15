8a7a5ead35f4a3b74b28afd817adfafbe9f4b826
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2024-07-15 09:28:09 +0700

    Finish day3

--- src/Day2/Solution.java
+++ src/Day2/Solution.java
@@ -25,7 +25,8 @@ class Result {
 
     public static void solve(double meal_cost, int tip_percent, int tax_percent) {
         // Write your code here
-
+        double result = (meal_cost + (meal_cost*tip_percent)/100 + (meal_cost*tax_percent)/100);
+            System.out.println(Math.round(result));
     }
 
 }
